2988c85758c33d430eb47a5f283a3f21239a43a9
Gaston Ojeda <gastonmiguel@live.com.ar>
2025-05-07 17:15:48 -0300

    firts commit

---
+++ src/app/ui/Layout/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return (
+        <>
+            <Image src="/img/logo.png" alt="Vosa" width="300" height="300" />
+        </>
+    )
+}
