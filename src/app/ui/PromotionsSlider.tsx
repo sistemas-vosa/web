f9d68ee7abbda7d31ca9c6950e22e7cf7409778f
gastonmiguel <gastonmiguel@live.com.ar>
2025-05-20 17:00:42 -0300

    add slider img

--- src/app/ui/PromotionsSlider.tsx
+++ src/app/ui/PromotionsSlider.tsx
@@ -4,35 +4,54 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/autoplay'
-import Image from "next/image";
+import Image from 'next/image'
 
-const images: string[] = [
-    '/img/01.png',
-    '/img/02.png',
-    // Podés agregar más imágenes si querés
+interface PromotionImage {
+    desktop: string
+    mobile: string
+}
+
+const images: PromotionImage[] = [
+    {
+        desktop: '/img/slider/01-desktop.png',
+        mobile: '/img/slider/01-mobile.png',
+    }
+    // Agregá más promociones si querés
 ]
 
 export default function PromotionsSlider() {
     return (
-        <div className="w-full h-[300px] md:h-[400px] rounded-lg overflow-hidden shadow-lg">
+        <div className="w-screen h-[300px] md:h-[400px] relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
             <Swiper
                 modules={[Autoplay]}
-                spaceBetween={30}
+                spaceBetween={0}
                 slidesPerView={1}
                 loop={true}
                 autoplay={{
                     delay: 5000,
-                    disableOnInteraction: false
+                    disableOnInteraction: false,
                 }}
             >
-                {images.map((image, index) => (
+                {images.map((img, index) => (
                     <SwiperSlide key={index}>
-                        <Image
-                            src={image}
-                            width={800}
-                            height={400}
-                            alt={`Promoción ${index + 1}`}
-                            className="w-full h-full object-cover" />
+                        <div className="relative w-full h-[300px] md:h-[400px]">
+                            {/* Mobile image */}
+                            <Image
+                                src={img.mobile}
+                                alt={`Promoción ${index + 1} mobile`}
+                                fill
+                                className="object-cover md:hidden"
+                                priority
+                            />
+                            {/* Desktop image */}
+                            <Image
+                                src={img.desktop}
+                                alt={`Promoción ${index + 1} desktop`}
+                                fill
+                                className="object-cover hidden md:block"
+                                priority
+                            />
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
